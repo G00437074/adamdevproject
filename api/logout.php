2d9accc50c665edb04bdd76e6648b93f72ed69b4
Adam Devlin <g00437074@atu.ie>
2025-11-05 11:18:23 +0000

    adding comments to api file

--- api/logout.php
+++ api/logout.php
@@ -1,6 +1,18 @@
 <?php
+// Start or resume the current session
+// This lets PHP access and manage the session data that was previously created
 session_start();
+
+// Remove all session variables
+// This clears any stored user data (like username, user ID, etc.)
 session_unset();
+
+// Completely destroy the session
+// This deletes the session file on the server and invalidates the session ID
 session_destroy();
+
+// Tell the browser that the response will be in JSON format
 header('Content-Type: application/json');
+
+// Send back a simple JSON response indicating the logout was successful
 echo json_encode(['success' => true]);
